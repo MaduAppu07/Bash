3f2cab38bc3f93776ef3b6b5de9c9e049bc05bbb
madu <madugurumath33@gmail.com>
2024-03-11 22:25:57 +0530

    if is added

---
+++ if.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter first number" num1
+read -p "Enter second number" num2
+a=num1 #$num1
+b=num2 #$num2
+if [ $num1 -eq $num2 ]
+	echo $num1 "is equal to" $num2
+else
+	echo $num1 "is not equal to :" $num2
+fi
